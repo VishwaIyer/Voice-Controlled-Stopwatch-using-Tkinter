85b03018a7a1f2c6bd638d319c4d43d8d680700d
Vishwa Iyer <vishwahariharaniyer93@gmail.com>
2020-04-28 11:49:37 +0530

    Add files via upload

---
+++ Stopwatch Tkinter.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import tkinter.font as TkFont
+from datetime import datetime
+
+def run():
+    current_time=datetime.now()
+    diff= current_time-start_time
+    txt_var.set('%d.%02d' %(diff.seconds,diff.microseconds//10000))
+    
+    if running:
+        root.after(20,run) #to reschedule after 20 milliseconds
+        
+def start():
+    global running
+    global start_time
+    
+    if not running:
+        running=True
+        start_time= datetime.now()
+        root.after(10,run)
+        
+def stop():
+    global running
+    running = False
+    
+def reset():
+    global start_time
+    start_time = datetime.now()
+    
+    if not running:
+        txt_var.set("0.00")
+        
+running=False
+start_time=None
+
+root=tk.Tk()
+root.geometry("500x200")
+root.title("Stopwatch")     
+
+txt_var= tk.StringVar()
+txt_var.set("0.00")
+
+
+fontStyle = TkFont.Font(family='Lucida Console',size=50)
+tk.Label(root,textvariable=txt_var,font= fontStyle).pack()
+
+tk.Button(root,text="Start",command=start).pack(fill='x')
+tk.Button(root,text='Stop',command = stop).pack(fill='x')
+tk.Button(root,text='Reset',command = reset).pack(fill='x')  
+tk.Button(root,text='Time Lapse',command=run).pack(fill='x')
+
+root.mainloop() 
+        
